58423300f182819642117cebcb66842918ffcb5b
Cource <jeffjacobjoy@gmail.com>
2019-05-20 00:45:05 +0530

    second commit

--- web/main.js
+++ web/main.js
@@ -3,7 +3,7 @@ let btn = document.getElementById('btn')
 let log = document.getElementById('log')
 
 function add() {
-  btn.innerText += eval(txt.value)
+  btn.innerText += txt.value
   log.innerHtml += txt.value + '<br>'
   txt.value = ''
 }
