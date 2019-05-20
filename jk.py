696001acd5f98404cf2c6430800663bf487613e9
Cource <jeffjacobjoy@gmail.com>
2019-05-20 14:39:08 +0530

    minor fixes to btn

--- jk.py
+++ jk.py
@@ -7,9 +7,11 @@ total = 0
 @eel.expose
 def main(txt_val):
     global total
-    total += eval(txt_val)
-    eel.add(total)
-    eel.log(txt_val + '<br>')
-
+    if not txt_val.isalpha():
+        total += eval(txt_val)
+        eel.add(total)
+        eel.log(txt_val + '<br>')
+    else:
+        print('[!]Invalid argument exeption: Dont input alphabets!')
 
 eel.start('index.html', size=(400, 1000))
